a182c789655cc3e5b84c474e94b5089b5cac6f2a
KelvinHere <kel@DESKTOP-N5MBM51.localdomain>
2022-02-11 10:19:11 +0000

    Feat: Change main.py to take user input.  DOCS: Update README.MD with more information

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import numpy as np
 
-num = np.random.randint(100)
-print(f"Random number created inside docker container = {num}")
+while True:
+    num = np.random.randint(100)
+    print(f"\nRandom number created inside docker container = {num}")
+    again = input("Enter Y to generate another or any other key to quit - ")
+    if not again:
+        break
+    if again[0].lower() != 'y':
+        break
